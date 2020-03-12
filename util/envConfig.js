ee5fccd2096944404682c95c7e62a21565437a43
xiaodong Zhang <zhangxiaodong_01@126.com>
2020-03-12 16:35:41 +0800

    Update envConfig.js

--- util/envConfig.js
+++ util/envConfig.js
@@ -1,19 +1,19 @@
-let envConfig = [ 'harley', 'demo' ] // 站点名称
-let env
-//获取命令行参数 http://nodejs.cn/api/process.html#process_process_argv
-const argv = JSON.parse(process.env.npm_config_argv).original || process.argv
-const website = argv[argv.length - 1] ? argv[argv.length - 1].replace(/[^A-Za-z0-9]+/ig,"") : '' // 站点名称
+let envConfig = [] // 构建/启动 项目
+let projectName = [ 'harley', 'xinwangda', 'tcl', 'shougangpark', 'itl'] // 站点名称
 
-if(envConfig.find(item => item === website)){
-	envConfig = []
-	envConfig.push(website)
-	env = argv[argv.length - 2] ? argv[argv.length - 2] === 'serve' ? website : argv[argv.length - 2].replace(/[^A-Za-z0-9]+/ig,"") : '' // 环境
-}
-else {
-	env = website
-}
+const needLogin = ['xinwangda', 'tcl'] // 是否需要登录功能
+// 获取命令行参数 http://nodejs.cn/api/process.html#process_process_argv
+const argv = JSON.parse(process.env.npm_config_argv).original || process.argv
+const env = argv[argv.length - 1] ? argv[argv.length - 1].replace(/[^A-Za-z0-9]+/ig,"") : '' // 环境
+
+projectName.map(item => {
+	if(env.includes(item)) {
+		envConfig.push(item)
+	}
+})
 
 module.exports = {
 	envConfig,
+	needLogin,
 	env
 }
