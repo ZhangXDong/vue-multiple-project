45b3df175cdd991d7493f35cd7317e2113927067
xiaodong Zhang <zhangxiaodong_01@126.com>
2020-03-12 16:37:03 +0800

    Update htmlReplace.js

--- util/htmlReplace.js
+++ util/htmlReplace.js
@@ -14,7 +14,7 @@ var callbackFile = function( src,dst, name, filepath ){
         let regCss = new RegExp("\/css\/"+name+"",'g');
         let regJs = new RegExp("\/js\/"+name+"",'g');
         let htmlContent = data.toString().replace(regCss,`\.\/css\/${name}`).replace(regJs,`\.\/js\/${name}`);
-        fs.writeFile(dst,htmlContent,'utf8',function(error){
+        fs.writeFile(dst, htmlContent, 'utf8', function(error){
             if(error){
                 console.log(error);
                 return false;
@@ -36,9 +36,9 @@ var callbackFile = function( src,dst, name, filepath ){
 glob.sync( './dist/js/*.js').forEach((filepath,name) => {
     let fileNameList = filepath.split('.');
     let fileName = fileNameList[1].split('/')[3];// 多页面页面目录
-    let thisDirectory = `./dist/${fileName}/${fileName}.html`;// 多页面JS文件地存放址
-    let changeDirectory = `./dist/${fileName}/index.html`;// 多页面JS文件地存放址
+    let thisDirectory = `./dist/${fileName}/${fileName}.html`
+    let changeDirectory = `./dist/${fileName}/index.html`
     if(!fileName.includes('chunk-vendors')){
-        callbackFile(thisDirectory,changeDirectory,fileName,filepath)
+        callbackFile(thisDirectory, changeDirectory, fileName, filepath)
     }
 })
